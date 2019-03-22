1ca33a4440c33a8b1bbab3a715b4ab936b6819d8
MaddyTing <37033263+MaddyTing@users.noreply.github.com>
2019-03-22 10:07:32 -0700

    Update and rename asdf to calendar v2.py

--- calendar v2.py
+++ calendar v2.py
@@ -30,15 +30,14 @@ def checkin(x, y):
     if(y > 12):
         return(False)
 #DAY INVALID
-    if(y == 4 or y == 6 or y == 9 or y == 11):
-        if(x > 30):
-            return(False)
-        elif(x > 29 and y == 2 and leapyear == 1):
-            return(False)
-        elif(x > 28 and y == 2 and leapyear == 0):
-            return(False)
-        elif(x > 31):
-            return(False)
+    if(y == 4 or y == 6 or y == 9 or y == 11 and x > 30):
+        return(False)
+    elif(x > 29 and y == 2 and leapyear == 1):
+        return(False)
+    elif(x > 28 and y == 2 and leapyear == 0):
+        return(False)
+    elif(x > 31):
+        return(False)
             
 #LEAP YEAR???    
 def leap_year(x):
@@ -64,18 +63,23 @@ def number_of_days(y, z):
 #2
 def days_left(x, y, z):
     leapyear = leap_year(z)
-    for i in range(y - 1):
-        if(y == 4 or y == 6 or y == 9 or y == 11):
-            x = x + 30
-        elif(y == 2 and leapyear == 1):
-            x = x + 29
-        elif(y == 2 and leapyear == 0):
-            x = x + 28
-        else:
-            x = x + 31   
+    print("Start")
     print(x)
-    print(y)
-    print(z)
+    for i in range(y - 1):
+        if(i == 2 and leapyear == 1):
+            x = x + 29
+            print("+29")
+        elif(i == 2 and leapyear == 0):
+            x = x + 28
+            print("+28")
+        elif(i == 4 or i == 6 or i == 9 or i == 11):  #Backwards calendar
+            x = x + 30
+            print("+30")
+        else:
+            x = x + 31 
+            print("+31")
+    print("New X")
+    print(x)
     if(leapyear == 1):
         return(366 - x)
     elif(leapyear == 0):
@@ -104,9 +108,7 @@ menu = int(input("\nMenu: " + "\n1)\tCalculate the number of days in the given m
 
 #MENU ACTION
 if(menu == 1):
-    number_of_days(month, year)
     print(number_of_days(month, year))
 if(menu == 2):
-    days_left(day, month, year)
     print(days_left(day, month, year))
 
