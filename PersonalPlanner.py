455fa38be7bb5821bfddc1b1a5fd5555794f79cb
MaddyTing <37033263+MaddyTing@users.noreply.github.com>
2019-04-23 10:43:08 -0700

    Update PersonalPlanner.py

--- PersonalPlanner.py
+++ PersonalPlanner.py
@@ -42,52 +42,23 @@ def addtolist(eventname, month, day, year):
     daylist.append(day)
     yearlist.append(year)
  
-def monthconversion(month, i):
-    j = monthlist[i]
+def monthconversion(monthnum):
+    j = monthlist[monthnum]
     return monthname[j - 1]
 
+for x in range(len(eventlist)):
+        if(str(monthlist[x]).find(str(specificmonth)) > -1):
+            print(eventlist[x])
+
 def specmonth(specificmonth):
-    print("\n********** Events in " + monthconversion(specificmonth, i) + " **********")
+    print("\n********** Events in " + monthconversion(specificmonth) + " **********")
 
     for x in range(len(eventlist)):
         if(str(monthlist[x]).find(str(specificmonth)) > -1):
             print(eventlist[x])
-            print("Date: " + monthconversion(month, x) + " " + str(daylist[x]) + ", " + str(yearlist[x]))
+            print("Date: " + monthconversion(x) + " " + str(daylist[x]) + ", " + str(yearlist[x]))
 
-##################  MAIN
-'''
-eventname = input("Enter the event name: ")
-month = int(input("Month: "))
-day = int(input("Day: "))
-year = int(input("Year: "))
-enterevent = input("Do you want to enter another date? NO to stop. ")
-'''
-
-eventname = input("Enter the event name: ")
-month = int(input("Month: "))
-day = int(input("Day: "))
-year = int(input("Year: "))    
-#Checking validity of month, day, year
-if(month > 12 or month < 1):
-    month = 1
-if(year < 1):
-    year = 1
-if(month == 4 or month == 6 or month == 9 or month == 11 and day > 30):
-    day = 1
-if(month == 2 and day > 29):
-    day = 1
-if(day < 1 or day > 31):
-        day = 1
-addtolist(eventname, month, day, year)
-
-enterevent = input("Do you want to enter another date? NO to stop. ")
-
-while(enterevent != "NO"):
-    eventname = input("Enter the event name: ")
-    month = int(input("Month: "))
-    day = int(input("Day: "))
-    year = int(input("Year: "))
-    
+def addnewevent(eventname, month, day, year):
     #Checking validity of month, day, year
     if(month > 12 or month < 1):
         month = 1
@@ -100,14 +71,34 @@ while(enterevent != "NO"):
     if(day < 1 or day > 31):
         day = 1
     addtolist(eventname, month, day, year)
+
+def printallevents():
+    print("******************** List of Events ********************")
+    for i in range(len(eventlist)):
+        print(eventlist[i])
+        print("Date: " + monthconversion(i) + " " + str(daylist[i]) + ", " + str(yearlist[i]))
+
+##################  MAIN
+
+eventname = input("Enter the event name: ")
+month = int(input("Month: "))
+day = int(input("Day: "))
+year = int(input("Year: "))
+
+addnewevent(eventname, month, day, year)
+enterevent = input("Do you want to enter another date? NO to stop. ")
+
+while(enterevent != "NO"):
+    eventname = input("Enter the event name: ")
+    month = int(input("Month: "))
+    day = int(input("Day: "))
+    year = int(input("Year: "))
     
+    addnewevent(eventname, month, day, year)
     enterevent = input("Do you want to enter another date? NO to stop. ")
 
 #### ONCE FINISHED
-print("******************** List of Events ********************")
-for i in range(len(eventlist)):
-    print(eventlist[i])
-    print("Date: " + monthconversion(month, i) + " " + str(daylist[i]) + ", " + str(yearlist[i]))
+printallevents()
 
 specificmonth = int(input("What month would you like to see? (Enter the month number) "))
 specmonth(specificmonth)
